baf63922ad7c7fe984f8e71426a63917a3e5e76f
george <georgeobiero2@gmail.com>
2023-12-04 20:23:04 +0200

    chore: install docker

--- docker/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-if [ ! -d $HOME/.docker/cli-plugins/ ]; then
-  mkdir "$HOME/.docker/cli-plugins"
-fi
-
-ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
-ln -sfn $(brew --prefix)/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
